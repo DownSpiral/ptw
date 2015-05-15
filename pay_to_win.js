e2b5a7080e41b3345c15bd2fc93f54283a17fd0a
DownSpiral <eric.d.wishart@gmail.com>
2015-05-15 00:19:55 -0700

    Add login support binding scores to users

--- pay_to_win.js
+++ pay_to_win.js
@@ -1,29 +1,49 @@
-PlayersList = new Mongo.Collection('players');
-
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+  //score starts at 0
+  Session.setDefault('score', 0);
 
   Template.button_tmpl.helpers({
-    counter: function () {
-      return Session.get('counter');
+    score: function () {
+      var user = Meteor.user();
+      return user === null ? Session.get('score') : user.profile.score;
     }
   });
 
   Template.button_tmpl.events({
+    // increment the counter when button is clicked
     'click .button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+      var user = Meteor.user();
+      if (user === null) {
+        Session.set('score', Session.get('score') + 1);
+      } else {
+        Meteor.users.update({
+          _id: Meteor.userId()
+        }, {
+          $set: {'profile.score': user.profile.score + 1 }
+        });
+      }
     }
   });
 
   Template.leaderboard.player = function(){
-    return PlayersList.find();
+    return Meteor.users.find({}, {
+      sort: {'profile.score': -1, name: 1}
+    });
   }
+
+  Accounts.ui.config({
+    passwordSignupFields: 'USERNAME_AND_EMAIL'
+  });
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+  });
+
+  Accounts.onCreateUser(function(options, user) {
+    user.profile = {};
+    user.profile.score = 0;
+    return user;
   });
 }
